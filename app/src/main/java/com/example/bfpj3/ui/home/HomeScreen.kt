49e088146a50e46ed32d317c5a5719d4288155f3
Yuxiang Liu <yl26417p@pace.edu>
2024-01-26 22:38:39 -0500

    login and signup auth added

--- app/src/main/java/com/example/bfpj3/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/bfpj3/ui/home/HomeScreen.kt
@@ -1,8 +1,12 @@
 package com.example.bfpj3.ui.home
 
+import android.widget.Toast
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavController
 
 @Composable
-fun Home() {
-
+fun HomeScreen(navController: NavController) {
+    Text(text= "Home Screen is here")
 }
--- app/src/main/java/com/example/bfpj3/ui/navigation/BottomNavigation.kt
+++ app/src/main/java/com/example/bfpj3/ui/navigation/BottomNavigation.kt
@@ -1,6 +1,8 @@
 package com.example.bfpj3.ui.navigation
 
+import android.widget.Toast
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavController
 
 @Composable
