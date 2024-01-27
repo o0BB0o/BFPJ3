176ccd7093227bf84e3dcb7eb70744015f78b10b
Qihou0812 <qihouzhang0812@gmail.com>
2024-01-26 17:30:31 -0800

    The basic implementation for login and register UI.

--- app/src/main/java/com/example/bfpj3/ui/register/RegisterScreen.kt
+++ app/src/main/java/com/example/bfpj3/ui/register/RegisterScreen.kt
@@ -1,8 +1,74 @@
 package com.example.bfpj3.ui.register
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.bfpj3.ui.login.Login
+import com.example.bfpj3.ui.theme.BFPJ3Theme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Register() {
-
+fun Register(navController: androidx.navigation.NavController) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var displayName by remember { mutableStateOf("") }
+    var curLocation by remember { mutableStateOf("") }
+    Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center, modifier = Modifier.fillMaxSize()){
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") },
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text(text = "Password") },
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(
+            value = displayName,
+            onValueChange = { displayName = it },
+            label = { Text("Display Name") },
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(
+            value = curLocation,
+            onValueChange = { curLocation = it },
+            label = { Text("Current Location") },
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { navController.navigate("Login")}) {
+            Text(text ="Register")
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun LoginViewPreview() {
+    BFPJ3Theme {
+        Register(navController = androidx.navigation.compose.rememberNavController())
+    }
 }
